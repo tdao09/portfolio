3373d1203dfd9902e5f03a4eb28c8219b7daf08e
tdao09 <tdao09@uw.edu>
2024-03-05 19:31:37 -0800

    resume added

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,15 @@
 
 import Navbar from '../components/navbar.js';
-import Footer from '../components/footer.js'
+import Footer from '../components/footer.js';
+
+import '../css/resume.css';
+
 export default function Resume() {
     return(
         <section>
             <Navbar />
-            <h1>Resume</h1>
+            <h1>Resume (reminder to change to most up to date)</h1>
+            <iframe className="pdf-file" src="./assets/Travon Dao Resume.pdf" />
             <Footer />
         </section>
     );
