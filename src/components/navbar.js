f8909e70ea5893e3b87767b50a4b8ee56f11ff92
tdao09 <tdao09@uw.edu>
2024-03-20 15:50:17 -0700

    hamburger open menu fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,16 +2,14 @@
 import '../css/navbar.css';
 import { NavLink } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Navbar() {
     const [showNav, toggleNav] = useState(false);
-
     function handleHamburgerTap() {
         toggleNav(!showNav);
     }
-
-
+  
     // <li><NavLink to="/resume">Resume</NavLink></li> 
     return(
         <nav className="navbar">
@@ -22,8 +20,12 @@ export default function Navbar() {
                     <li><NavLink to="/projects">Projects</NavLink></li>
                     <li><a className="resume-button" href="./assets/travon-dao-resume.pdf">Resume</a></li>
                 </ul>
+                
                 <FaBars className={`hamburger ${showNav ? "hamburger-open" : ""}`} alt="More options/
                         hamburger icon" onClick={handleHamburgerTap}/>
         </nav>
     );
 }
+
+
+// booleanName ? thisvalue : thatvalue
