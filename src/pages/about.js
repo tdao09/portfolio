8141fbde7c1ce1e1c8284805bc3ed51937d77dee
tdao09 <tdao09@uw.edu>
2024-04-01 17:47:18 -0700

    fixed image on about

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,13 @@ export default function About() {
         <h1 className="title">Human-Centered Designer & Full-Stack Developer</h1>
         <div className="about-container">
             <div className="school-experience">
+            <img className="self-portrait" src="./img/travon.jpg" alt="Travon Dao"/>
                 <p>I graduated from the University of Washington, Seattle where I studied <strong>Informatics</strong> and <strong>Psychology</strong>. Given my multidisciplinary background, I am equipped to design for people with careful consideration for how they think, process information, and make decisions.</p>
 
                 <p>I have experience in scientific research, UX design & research, full-stack web development, product management, data science, and data modeling. With my wide range of skills, I have designed and developed information systems such as an informational food website, an accessible chatting application designated for older audiences, and a full-stack website where college students can rate and review study spots around their college campus. </p>
 
                 <p>I am extremely passionate in the empowerment of marginalized communities. As the older brother of a person with autism, I dedicate myself towards finding ways to make life <strong>easier</strong> for others through creative problem-solving and design.</p>
            </div>
-           <div className="image">
-                <img className="self-portrait" src="./img/travon.jpg" alt="Travon Dao"/>
-           </div>
-    
         </div>
         
         <h1 className="lab-heading">Professional Experience</h1>
