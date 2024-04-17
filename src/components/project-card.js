a8e7be2e54ec8bbc1859577cc8a1861b25934625
tdao09 <tdao09@uw.edu>
2024-04-17 15:23:54 -0700

    fixes

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -9,11 +9,12 @@ export default function ProjectCard(props) {
    }
 
     return(
-        <div onClick={handleCardClick} className="project-card" id={`${props.data.name}-project`}>
+        <div className="project-card" id={`${props.data.name}-project`}>
             <img src={props.data.thumbnail} alt={`${props.data.name} project card`}/>
             <h2 className="project-title">{props.data.name}</h2>
             <p className="project-caption">{props.data.caption}</p>
             <p className="project-description">{props.data.description}</p>
+            <button className="btn btn-dark" onClick={handleCardClick}>Read More</button>
         </div>
     );
 }
