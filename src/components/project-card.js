4d4d3e3a08146c0f4bcd564a892f06b7ae0b582f
tdao09 <tdao09@uw.edu>
2024-06-03 18:40:08 -0700

    projects

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,5 +1,7 @@
 import '../css/project-card.css';
 import { useNavigate } from 'react-router-dom';
+import {FaGithub} from 'react-icons/fa'
+
 // props.data.thumbnail-url
 export default function ProjectCard(props) {
     const navigation = useNavigate();
@@ -12,9 +14,14 @@ export default function ProjectCard(props) {
         <div className="project-card" id={`${props.data.name}-project`}>
             <img src={props.data.thumbnail} alt={`${props.data.name} project card`}/>
             <h2 className="project-title">{props.data.name}</h2>
-            <p className="project-caption">{props.data.caption}</p>
+           
             <p className="project-description">{props.data.description}</p>
-            <button className="btn btn-dark" onClick={handleCardClick}>Read More</button>
+            <button className="btn btn-dark project-button" onClick={handleCardClick}>Read More</button>
+            
+           {props.data.github?  <a href={props.data.github} target="_blank" rel="noreferrer"><FaGithub className="gh-logo" alt="Github logo"/></a>: null}
+           
         </div>
     );
 }
+
+//  <p className="project-caption">{props.data.caption}</p>
